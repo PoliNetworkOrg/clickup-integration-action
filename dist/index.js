09bb8abeac5876fd82cfc2fa69c1f752d5654196
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-02-06 22:21:20 +0100

    log

--- dist/index.js
+++ dist/index.js
@@ -47,16 +47,16 @@ function run() {
             const clickupToken = core.getInput('clickup_api_key');
             const problemFolderId = core.getInput('problem_folder_id');
             const featureFolderId = core.getInput('feature_folder_id');
-            core.debug('Dumping inputs');
-            core.debug(clickupToken);
-            core.debug(problemFolderId);
-            core.debug(featureFolderId);
-            core.debug('Dumping context');
-            core.debug(`Hello ${github.context.actor}!`);
-            core.debug(`The event name is ${github.context.eventName}.`);
-            core.debug(`The ref is ${github.context.ref}.`);
-            core.debug(`The workflow is ${github.context.workflow}.`);
-            core.debug(`The action is ${github.context.action}.`);
+            console.log('Dumping inputs');
+            console.log(clickupToken);
+            console.log(problemFolderId);
+            console.log(featureFolderId);
+            console.log('Dumping context');
+            console.log(`Hello ${github.context.actor}!`);
+            console.log(`The event name is ${github.context.eventName}.`);
+            console.log(`The ref is ${github.context.ref}.`);
+            console.log(`The workflow is ${github.context.workflow}.`);
+            console.log(`The action is ${github.context.action}.`);
             core.setOutput('clickup_api_key', clickupToken);
         }
         catch (error) {
