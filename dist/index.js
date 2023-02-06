b1d78b96879905760d9b962d45014aa528c48ca3
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-02-06 21:49:06 +0100

    star import

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,29 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,33 +38,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const clickupToken = core_1.default.getInput('clickup_api_key');
-            const problemFolderId = core_1.default.getInput('problem_folder_id');
-            const featureFolderId = core_1.default.getInput('feature_folder_id');
-            core_1.default.debug('Dumping inputs');
-            core_1.default.debug(clickupToken);
-            core_1.default.debug(problemFolderId);
-            core_1.default.debug(featureFolderId);
-            core_1.default.debug('Dumping context');
-            core_1.default.debug(`Hello ${github_1.default.context.actor}!`);
-            core_1.default.debug(`The event name is ${github_1.default.context.eventName}.`);
-            core_1.default.debug(`The ref is ${github_1.default.context.ref}.`);
-            core_1.default.debug(`The workflow is ${github_1.default.context.workflow}.`);
-            core_1.default.debug(`The action is ${github_1.default.context.action}.`);
-            core_1.default.setOutput('clickup_api_key', clickupToken);
+            const clickupToken = core.getInput('clickup_api_key');
+            const problemFolderId = core.getInput('problem_folder_id');
+            const featureFolderId = core.getInput('feature_folder_id');
+            core.debug('Dumping inputs');
+            core.debug(clickupToken);
+            core.debug(problemFolderId);
+            core.debug(featureFolderId);
+            core.debug('Dumping context');
+            core.debug(`Hello ${github.context.actor}!`);
+            core.debug(`The event name is ${github.context.eventName}.`);
+            core.debug(`The ref is ${github.context.ref}.`);
+            core.debug(`The workflow is ${github.context.workflow}.`);
+            core.debug(`The action is ${github.context.action}.`);
+            core.setOutput('clickup_api_key', clickupToken);
         }
         catch (error) {
             if (error instanceof Error)
-                core_1.default.setFailed(error.message);
+                core.setFailed(error.message);
         }
     });
 }
